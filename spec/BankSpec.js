cf971bee854c210da5ba5b0c7ad1723467b7f3e7
Liz Daly <liz7daly@gmail.com>
2020-03-23 12:21:59 +0000

    Setup Jasmine for testing

---
+++ spec/BankSpec.js
@@ -0,0 +1,20 @@
+describe("Bank", function() {
+  var bank;
+
+  beforeEach(function() {
+    bank= new Bank();
+  });
+
+  it("statement", function() {
+   
+  });
+
+  describe("scenario", function() {
+    beforeEach(function() {
+    });
+
+    it("statement", function() {
+    });
+  });
+  
+});
