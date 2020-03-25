aafc7fe94800ecc4e27bb454c643ac16b237096c
Liz Daly <liz7daly@gmail.com>
2020-03-25 15:21:35 +0000

    Add account history records to transactions
    Adds an additional step to `Account.credit` method - which adds a new record to
    an array of account history.

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -4,14 +4,40 @@ describe("Account", function() {
   beforeEach(function() {
     account = new Account();
   });
+    
+    it("can return a balance of 0", function() {
+      expect(account.getBalance()).toBe(0);
+    });
 
-  it("can return a balance of 0", function() {
-    expect(account.getBalance()).toBe(0);
+    it("account history is an empty array", function() {
+      expect(account.getAccountHistory()).toEqual([])
+    });
+ 
+
+
+  describe("when account is credited", function() {
+    beforeEach(function() {
+      account.credit(1);
+    });
+
+    it("balance increases when account is credited", function() {
+      expect(account.getBalance()).toBe(1);
+    });
+
+    it("adds a record to the account history", function() {
+      expect(account.getAccountHistory().length).toEqual(1)
+    });
+
+    it("adds a credit amount to the account history", function() {
+      expect(account.getAccountHistory()[0].credit).toEqual(1)
+    });
+
+    it("adds the new balance to the account history", function() {
+      expect(account.getAccountHistory()[0].balance).toEqual(1)
+    });
   });
 
-  it("balance increases when account is credited", function() {
-    account.credit(1);
-    expect(account.getBalance()).toBe(1);
-  })
   
+  
+
 });
--- src/account.js
+++ src/account.js
@@ -1,11 +1,18 @@
 function Account() {
   this._balance = 0
+  this._accountHistory = []
 };
 
 Account.prototype.getBalance = function() {
   return this._balance
 };
 
+Account.prototype.getAccountHistory = function() {
+  return this._accountHistory
+}
+
 Account.prototype.credit = function(amount) {
-  return this._balance += amount;
+  this._balance += amount;
+  return this._accountHistory.push({date: Date.now, credit: amount, debit: null, balance: this._balance})
 };
+
